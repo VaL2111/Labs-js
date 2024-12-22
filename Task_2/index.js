84bd3b0d470b9e849ecb3d7554fda8bb2e039cc9
Valentyn Bahmatskyi <valentunbaxc@gmail.com>
2024-12-22 23:44:10 +0200

    Added support for parallelism.

--- Task_2/index.js
+++ Task_2/index.js
@@ -7,6 +7,27 @@ function promiseFilter(array, callback) {
 	return Promise.all(promises).then(results => results.filter(item => item !== null));
 }
 
+async function promiseFilterWithLimit(array, callback, limit) {
+	const results = [];
+	const executing = [];
+
+	for (const item of array) {
+		const promise = callback(item).then(result => (result ? item : null));
+		results.push(promise);
+		executing.push(promise);
+
+		if (executing.length >= limit) {
+			const completedPromise = await Promise.race(executing);
+			const index = executing.findIndex(p => p === completedPromise);
+			executing.splice(index, 1);
+		}
+	}
+
+	const resolvedResults = await Promise.all(results);
+	const filteredResolvedResults = resolvedResults.filter(item => item !== null);
+	return filteredResolvedResults;
+}
+
 function isEvenPromise(num) {
 	return new Promise(resolve => {
 		setTimeout(() => resolve(num % 2 === 0), 500);
@@ -28,5 +49,13 @@ async function demoWithAsyncAwait() {
 	console.log("Результат фільтрування через Async/Await:", filteredNumbers);
 }
 
+async function demoWithParallelism() {
+	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+	const filteredNumbers = await promiseFilterWithLimit(numbers, isEvenPromise, 2);
+	console.log("Результат фільтрування з паралельністю:", filteredNumbers);
+}
+
 demoWithPromise();
 demoWithAsyncAwait();
+demoWithParallelism()
